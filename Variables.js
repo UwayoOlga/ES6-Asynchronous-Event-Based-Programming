ffa2a8116202c01837b9620068ac439b2e626a40
UwayoOlga <uwayoolga@gmail.com>
2025-01-21 16:27:38 +0200

    variable declaration with let

---
+++ Variables.js
@@ -0,0 +1,7 @@
+//Declare a variable firstname and initialize it with the value 'Lata'.
+
+let firstname = 'Lata';
+
+//Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
+let flower = 'rose';
+let tree = 'maple';
