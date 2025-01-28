12c9fbf93ad3764e33fd5467380e117a32b12540
UwayoOlga <uwayoolga@gmail.com>
2025-01-28 17:56:33 +0200

    finding the maximum number in the array

--- Call, Bind, Apply/Maxnum.js
+++ Call, Bind, Apply/Maxnum.js
@@ -3,6 +3,6 @@ use the apply method to find the maximum number in the array. */
 
 const numbers = [34, 12, 57, 28, 91];
 
-const maxNumber = Math.max.apply(null, numbers);
+const maxNumber = Math.max.apply(null, numbers); // passed null as math..max() is a standalone in this case 
 
 console.log(maxNumber);  
