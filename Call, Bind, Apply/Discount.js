a79bb99d7798944f5c38d7e8ff13ed90f1c8c38d
UwayoOlga <uwayoolga@gmail.com>
2025-01-28 19:54:55 +0200

    using bind method to create a new function

---
+++ Call, Bind, Apply/Discount.js
@@ -0,0 +1,17 @@
+/*Write a function that calculates the discounted price of a product with a fixed discount percentage. 
+Use the bind method to create a new function with a discount of 10%. 
+Calculate the discounted price for a product with an initial price of $50 and $100 */
+ // Step 1: Create the base function to calculate the discounted price
+
+ function calculateDiscountedPrice(price, discount) {
+    return price - (price * discount / 100);
+  }
+   
+  const discount10 = calculateDiscountedPrice.bind(null, undefined, 10);
+   
+  const price1 = 50;
+  const price2 = 100;
+  
+  console.log(discount10(price1)); 
+  console.log(discount10(price2)); 
+  
