1e62f62c11a95f54c0c21eea17a45c34cfc37ec1
UwayoOlga <uwayoolga@gmail.com>
2025-04-16 17:02:09 +0200

    async&await

---
+++ Asynchronous/sync&awaitII.js
@@ -0,0 +1,25 @@
+function checkUserAccess(isAuthorized) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (isAuthorized) {
+          resolve("Access granted ✅");
+        } else {
+          reject("Access denied ❌");
+        }
+      }, 1500); 
+    });
+  }
+  
+  async function authorizeUser() {
+    console.log("Checking user access...");
+    try {
+      const result = await checkUserAccess(true); n
+      console.log(result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    console.log("Authorization process finished.");
+  }
+  
+  authorizeUser();
+  
